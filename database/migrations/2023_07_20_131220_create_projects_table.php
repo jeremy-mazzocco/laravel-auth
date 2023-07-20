3e1bac364f886a1fd434381d5d4138cf267bce61
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-07-20 14:17:25 +0100

    first commit

---
+++ database/migrations/2023_07_20_131220_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 128);
+            $table->string('languages', 64);
+            $table->collaborators('collaborators');
+            $table->date_create();
+            $table->date_finished();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
