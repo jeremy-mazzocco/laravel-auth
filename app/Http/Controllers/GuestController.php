08f52a69720ba42cf9c5fa056fad988c8b849199
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-07-23 17:42:45 +0100

    add guest controller

---
+++ app/Http/Controllers/GuestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class GuestController extends Controller
+{
+    public function index()
+    {
+        return view('guest.index');
+    }
+}
